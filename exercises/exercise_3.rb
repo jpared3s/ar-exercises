5ab46307eca1fb5410e61746d17cdedd69564ef7
jpared3s <jparedes.wall@gmail.com>
2023-03-07 16:40:58 -0500

    exercise_3

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -6,3 +6,13 @@ puts "Exercise 3"
 puts "----------"
 
 # Your code goes here ...
+class Store < ActiveRecord::Base
+end
+
+
+@store3 = Store.find(3)
+
+@store3.destroy
+
+
+puts "Total number of stores: #{Store.count}"
