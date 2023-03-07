e958e5793124fe210a1823ebdf1b1f96fc9323c6
jpared3s <jparedes.wall@gmail.com>
2023-03-07 17:09:33 -0500

    exercises_5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,15 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+class Store < ActiveRecord::Base
+end
+
+total_revenue = Store.sum(:annual_revenue)
+puts "Total Revenue: $#{total_revenue}"
+
+average_revenue = Store.average(:annual_revenue)
+puts "Average Annual Revenue: $#{average_revenue}"
+
+
+num_high_revenue_stores = Store.where("annual_revenue >= ?", 1000000).count
+puts "Number of high revenue stores: #{num_high_revenue_stores}"
